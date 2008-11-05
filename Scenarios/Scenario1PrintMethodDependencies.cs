bd30e39a153dbfc491b8e06c59aed9ba3b396aba
Torbjörn Gyllebring <i.am@drunkencoder.com>
2008-11-05 21:46:58 +0000

    First Scenarion added.

---
+++ Scenarios/Scenario1PrintMethodDependencies.cs
@@ -0,0 +1,19 @@
+namespace Pencil.Scenarios.Scenario1
+{
+	class PrintMethodRefrences
+	{
+		public void Run(string assemblyPath)
+		{
+			var assembly = AssemblyLoader.Load(assemblyPath);
+			foreach(var module in assembly.Modules)
+				foreach(var type in module.Types)
+					foreach(var method in type.Methods)
+					{
+						Console.WriteLine("{0} calls:", method);
+						foreach(var instruction in method.Body)
+							if(instruction.IsCall)
+								Console.WriteLine(instruction.Operand);
+					}
+		}
+	}
+}
