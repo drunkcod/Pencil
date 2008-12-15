769f55d232b24b5c8ff5ac2d02fc1525170e82e8
Torbjörn Gyllebring <i.am@drunkencoder.com>
2008-12-15 22:50:12 +0000

    MethodArguments sample begun.

---
+++ Samples/MethodArguments.fsx
@@ -0,0 +1,23 @@
+﻿#light
+
+#r "..\Build\Debug\Pencil.dll"
+
+open System
+open Pencil.Core
+
+let handler = { 
+    new obj() 
+        interface IHandler with
+            member self.BeginAssembly x = ()
+            member self.EndAssembly() = ()
+            member self.BeginModule x = ()
+            member self.EndModule() = ()
+            member self.BeginType x = ()
+            member self.EndType() = ()
+            member self.BeginMethod x = Console.WriteLine("{0}", x.Name)
+            member self.EndMethod() = ()
+ }
+    
+let reader = AssemblyReader(handler)
+
+reader.Read(AssemblyLoader.LoadFrom("..\Build\Debug\Pencil.dll"))
