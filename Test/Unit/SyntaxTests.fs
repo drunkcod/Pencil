98c2761f27dae3036655d193a1d6c63b550d79e0
Torbjörn Gyllebring <i.am@drunkencoder.com>
2009-02-01 21:25:21 +0000

    Syntax simplification to remove parens. Wont added for negative match.

---
+++ Test/Unit/SyntaxTests.fs
@@ -0,0 +1,15 @@
+#light
+
+namespace Pencil.Test.Unit
+
+open System
+open Pencil.Unit
+open NUnit.Framework
+
+[<NUnitFixture>]
+type SyntaxTests() =
+    [<Test>]
+    member this.Wont_should_use_MatchMessage() =
+        (1 |> Wont Equal 1) {new ITestResult with
+            member this.Success() = ()
+            member this.Failiure e = (e.Message |> Should Equal "Actual was 1")} 
