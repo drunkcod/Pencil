8e3a099518f89842e67fa1e293980ec94b5f19db
Torbjörn Gyllebring <i.am@drunkencoder.com>
2009-02-10 22:29:54 +0000

    Whoops, missed a file.

---
+++ Test/Build/Tasks/ExecTaskBaseTests.cs
@@ -0,0 +1,37 @@
+namespace Pencil.Test.Build.Tasks
+{
+    using Pencil.Build;
+    using Pencil.Build.Tasks;
+    using Pencil.IO;
+    using Pencil.Test.Stubs;
+    using NUnit.Framework;
+    
+    [TestFixture]
+    public class ExecTaskBaseTests
+    {
+        class TestTask : ExecTaskBase
+        {
+            public TestTask(IExecutionEnvironment platform): base(platform)
+            {}
+            protected override Path GetProgramCore(){ return new Path("TestTask"); }
+		    protected override  string GetArgumentsCore(){ return string.Empty; }
+        }
+    
+        [Test]
+        public void Execute_should_call_wait_for_exit_before_ExitCode()
+        {//Since mono is broken and won't give us the ExitCode otherwise.
+            var process = new ProcessStub();
+            var waitForExitCalled = false;
+            process.WaitForExitHandler = () => waitForExitCalled = true;
+            process.GetExitCodeHandler = () =>
+            { 
+                Assert.IsTrue(waitForExitCalled);
+                return 0;
+            };
+            var platform = new ExecutionEnvironmentStub();
+            platform.RunHandler = (program, args, handler) => handler(process);
+            var task = new TestTask(platform);
+            task.Execute();
+        }
+    }
+}
