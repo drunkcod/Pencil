94cd10eacdc277eb921391e3fc46c71a0382e77c
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-01-07 22:47:07 +0100

    DefaultFormatter now handles generic methods and arguments.

--- Source/Core/DefaultFormatter.cs
+++ Source/Core/DefaultFormatter.cs
@@ -8,7 +8,9 @@ namespace Pencil.Core
     {
         public string Format(MethodInfo method) {
             var signature = new StringBuilder(Format(method.ReturnType));
-            signature.AppendFormat(" {0}::{1}(", Format(method.DeclaringType), method.Name);
+            signature.AppendFormat(" {0}::{1}", Format(method.DeclaringType), method.Name);
+            AppenGenericArguments(signature, method);
+            signature.Append('(');
             var format = "{0}";
             foreach(var item in method.GetParameters()) {
                 signature.AppendFormat(format, Format(item.ParameterType));
@@ -17,6 +19,22 @@ namespace Pencil.Core
             return signature.Append(')').ToString();
         }
 
-        string Format(System.Type type) { return type.FullName; }
+        string Format(System.Type type){
+            var fullName = type.FullName;
+            if(fullName == null)
+                return type.Name;
+            return fullName; 
+        }
+
+        void AppenGenericArguments(StringBuilder signature, MethodInfo method) {
+            if(method.IsGenericMethod) {
+                var format = "<{0}";
+                foreach(var item in method.GetGenericArguments()) {
+                    signature.AppendFormat(format, Format(item));
+                    format = ", {0}";
+                }
+                signature.Append('>');
+            }
+        }
     }
 }
--- Test/Core/DefaultFormatterTests.cs
+++ Test/Core/DefaultFormatterTests.cs
@@ -10,7 +10,9 @@ namespace Pencil.Test.Core
 {
     class MyClass
     {
-        public void Action(){}
+        public void Generic<T>() { }
+        public void Generic2<T0,T1>() { }
+        public void Action() { }
         public void Action<T>(T item) { }
         public void Action<T0,T1>(T0 arg0, T1 arg1) { }
     }
@@ -28,16 +30,23 @@ namespace Pencil.Test.Core
         public IEnumerable<TestCaseData> MethodTests() {
             return Tests(
                 CheckFormat<MyClass>(x => x.Action(), "System.Void Pencil.Test.Core.MyClass::Action()"),
-                CheckFormat<MyClass>(x => x.Action(42), "System.Void Pencil.Test.Core.MyClass::Action(System.Int32)"),
-                CheckFormat<MyClass>(x => x.Action(42, ""), "System.Void Pencil.Test.Core.MyClass::Action(System.Int32, System.String)"));
+                CheckFormat<MyClass>(x => x.Action(42), "System.Void Pencil.Test.Core.MyClass::Action<System.Int32>(System.Int32)"),
+                CheckFormat<MyClass>(x => x.Action(42, ""), "System.Void Pencil.Test.Core.MyClass::Action<System.Int32, System.String>(System.Int32, System.String)"),
+                CheckFormat(typeof(MyClass).GetMethod("Generic"), "System.Void Pencil.Test.Core.MyClass::Generic<T>()"),
+                CheckFormat(typeof(MyClass).GetMethod("Generic2"), "System.Void Pencil.Test.Core.MyClass::Generic2<T0, T1>()"));
         }
 
         T[] Tests<T>(params T[] tests) { return tests; }
 
         TestCaseData CheckFormat<T>(Expression<Action<T>> func, string expected){
             var method = (func.Body as MethodCallExpression).Method;
+            return CheckFormat(method, expected);
+        }
+
+        TestCaseData CheckFormat(MethodInfo method, string expected) {
             var data = new TestCaseData(method, expected);
             return data.SetName("\"" + method + "\"");
         }
+
     }
 }
