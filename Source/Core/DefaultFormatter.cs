a8e1e78776859dcfe27e5b3f7fd3c8e48154edf3
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-01-07 19:57:36 +0100

    DefaultFormatter promoted to own file.

---
+++ Source/Core/DefaultFormatter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Reflection;
+
+namespace Pencil.Core
+{
+    public class DefaultFormatter
+    {
+        public string Format(MethodInfo method) {
+            var signature = new StringBuilder(Format(method.ReturnType));
+            signature.AppendFormat(" {0}::{1}(", Format(method.DeclaringType), method.Name);
+            var format = "{0}";
+            foreach(var item in method.GetParameters()) {
+                signature.AppendFormat(format, Format(item.ParameterType));
+                format = ", {0}";
+            }
+            return signature.Append(')').ToString();
+        }
+
+        string Format(System.Type type) { return type.FullName; }
+    }
+}
--- Test/Core/DefaultFormatterTests.cs
+++ Test/Core/DefaultFormatterTests.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using System.Linq.Expressions;
+using Pencil.Core;
 using System.Reflection;
 
 namespace Pencil.Test.Core
@@ -11,33 +11,33 @@ namespace Pencil.Test.Core
     class MyClass
     {
         public void Action(){}
-    }
-
-    class DefaultFormatter
-    {
-        public string Format(MethodInfo method) {
-            return string.Format("{0} {1}::{2}()", method.ReturnType.FullName, method.DeclaringType.FullName, method.Name);
-        }
+        public void Action<T>(T item) { }
+        public void Action<T0,T1>(T0 arg0, T1 arg1) { }
     }
 
     [TestFixture]
     public class DefaultFormatterTests
     {
         DefaultFormatter formatter = new DefaultFormatter();
+
         [TestCaseSource("MethodTests")]
         public void MethodFormatting(MethodInfo method, string expected){
             Assert.That(formatter.Format(method), Is.EqualTo(expected));
         }
 
         public IEnumerable<TestCaseData> MethodTests() {
-            return Tests(CheckFormat<MyClass>(x => x.Action(), "System.Void Pencil.Test.Core.MyClass::Action()"));
+            return Tests(
+                CheckFormat<MyClass>(x => x.Action(), "System.Void Pencil.Test.Core.MyClass::Action()"),
+                CheckFormat<MyClass>(x => x.Action(42), "System.Void Pencil.Test.Core.MyClass::Action(System.Int32)"),
+                CheckFormat<MyClass>(x => x.Action(42, ""), "System.Void Pencil.Test.Core.MyClass::Action(System.Int32, System.String)"));
         }
 
         T[] Tests<T>(params T[] tests) { return tests; }
 
         TestCaseData CheckFormat<T>(Expression<Action<T>> func, string expected){
-            
-            return new TestCaseData((func.Body as MethodCallExpression).Method, expected);
+            var method = (func.Body as MethodCallExpression).Method;
+            var data = new TestCaseData(method, expected);
+            return data.SetName("\"" + method + "\"");
         }
     }
 }
